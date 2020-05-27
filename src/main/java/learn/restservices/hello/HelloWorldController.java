8dad0fd8baceea5ccec9121142f2d1c7527900f6
ronak rabadiya <rabadiyaronak@gmail.com>
2020-05-27 19:44:46 +0530

    enable i18n with spring

--- src/main/java/learn/restservices/hello/HelloWorldController.java
+++ src/main/java/learn/restservices/hello/HelloWorldController.java
@@ -1,10 +1,19 @@
 package learn.restservices.hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 
 //	@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
 	@GetMapping("/helloworld1")
@@ -15,5 +24,15 @@ public class HelloWorldController {
 	@GetMapping("/helloworld-bean")
 	public UserDetails HelloWorldBean() {
 		return new UserDetails("fName", "lName", "citival");
+	}
+
+	@GetMapping("/hello-int")
+	public String getMessageInI18NFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+		return messageSource.getMessage("lable.hello", null, new Locale(locale));
+	}
+
+	@GetMapping("/hello-int2")
+	public String getMessageInI18NFormat() {
+		return messageSource.getMessage("lable.hello", null, LocaleContextHolder.getLocale());
 	}
 }
